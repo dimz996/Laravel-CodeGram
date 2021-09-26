b9f0e4a0cdf29d7bc36995da87c1d3ab58a8c8dd
Dimuthu Fernando <dimuthuchalani996@gmail.com>
2021-09-26 15:52:48 +0530

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+      <div class="col-3">
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.logo.com%2Fhotlink-ok%2Flogo-social.png&imgrefurl=https%3A%2F%2Flogo.com%2F&tbnid=-eDJED2XpumqjM&vet=12ahUKEwioiYmN2przAhXNu0sFHcWuCjAQMygEegUIARDcAQ..i&docid=MUnsBNeVrAyn1M&w=1200&h=630&q=logo&ved=2ahUKEwioiYmN2przAhXNu0sFHcWuCjAQMygEegUIARDcAQ" alt="">
+      </div>
+      <div class="col-9 pt-5">
+           <div><h4>CodeGram</h4></div>
+           <div class="d-flex">
+               <div class="pr-5"> <strong>153</strong> posts </div>
+               <div class="pr-5"><strong>23k</strong> followers</div>
+               <div class="pr-5"><strong>212</strong> posts</div>
+           </div>
+           <div class="pt-4 font-weight-bold" > CodeGram.org</div>
+           <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+      <div><a href="#" style="color:black">www.codegram.org</a></div>
+        </div>
+  </div>
+
+</div>
+@endsection
